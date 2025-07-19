e065918a51d17bb0385ae481a1e1893edc68f909
omyahackerrr <omyahacker01@gmail.com>
2025-07-19 09:12:15 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import telebot
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_USERNAME = os.getenv("OWNER_USERNAME", "unknown")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, f"Hello 👋\nThis bot is managed by @{OWNER_USERNAME}")
+
+bot.infinity_polling()
